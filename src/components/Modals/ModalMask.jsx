65ce72fefec0163b6a6ac5d2ab89a97a7dda186d
Syd Cristal <SydCristal@gmail.com>
2023-10-29 19:22:16 +0300

    666

--- src/components/Modals/ModalMask.jsx
+++ src/components/Modals/ModalMask.jsx
@@ -9,7 +9,7 @@ const StlModalMask = styled.div`
 		position: fixed;
 		left: 0;
 		width: 100%;
-		height: 100%;
+		height: 100vh;
 		z-index: 100;
 		background-color: rgba(0, 0, 0, 0.3);
 		display: flex;
