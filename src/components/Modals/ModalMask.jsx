57f2aa9a88587574a7e620818a4a7c90ac721f41
Syd Cristal <SydCristal@gmail.com>
2023-10-31 14:56:14 +0300

    666

--- src/components/Modals/ModalMask.jsx
+++ src/components/Modals/ModalMask.jsx
@@ -11,7 +11,7 @@ const StlModalMask = styled.div`
 		overflow: scroll;
 		left: 0;
 		width: 100%;
-		height: 100vh;
+		height: 100%;
 		min-height: 350px;
 		z-index: 100;
 		background-color: rgba(0, 0, 0, 0.3);
