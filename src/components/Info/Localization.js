13a85a33f8447001f4eecfa6790bb613055e4cc7
Syd Cristal <SydCristal@gmail.com>
2023-11-05 15:27:08 +0300

    Sloggan corrected again

--- src/components/Info/Localization.js
+++ src/components/Info/Localization.js
@@ -3,12 +3,12 @@
 export const l = new LocalizedStrings({
 		en: {
 				text: 'Sunset Road Beer is crafted on the Island of the Gods - your perfect journey companion, provides a sense of freedom and a taste of adventure.\n Belgian malts, German hops, and French yeasts mature and naturally carbonate our beer during 30 sunsets without artificial additives.\nWhether going on a trip or just enjoying your moment of relaxation - grab Sunset Road Beer, fill up your life with sunny and flavorful experience.',
-				slogan: 'Find your pleasing...',
+				slogan: 'Find your pleasing',
 				brewed: 'Brewed in Bali'
 		},
 		id: {
 				text: 'Sunset Road Beer dibuat di Pulau Dewata - teman perjalanan Anda yang sempurna, memberikan rasa kebebasan dan cita rasa petualangan.\nMalt Belgia, hop Jerman, dan ragi Prancis matang dan mengkarbonasi bir kami secara alami selama 30 matahari terbenam tanpa bahan tambahan buatan.\nBaik sedang bepergian atau sekadar menikmati momen relaksasi Anda - nikmati Sunset Road Beer, penuhi hidup Anda dengan pengalaman yang cerah dan beraroma.',
-				slogan: 'Temukan kesenangan Anda...',
+				slogan: 'Temukan kesenangan Anda',
 				brewed: 'Dibuat di Bali'
 		}//,
 		//ru: {
