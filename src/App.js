8ba5e5b76521238eda5b6a0b3fa93fdea83283d2
Syd Cristal <SydCristal@gmail.com>
2023-11-14 03:38:16 +0800

    translation fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Layout from './components/Layout'
 import styled from 'styled-components'
 import { useAgeConfirmationContext, useScreenContext, useContactFormContext } from './Contexts'
 import { ModalMask } from './components/Modals'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 const StlApp = styled.div`
 		min-height: 100vh;
@@ -10,7 +10,7 @@ const StlApp = styled.div`
 `
 
 const AdultContent = styled.div`
-		${({ $blur, overflow, $translateContent }) => {
+		${({ $blur, overflow, $shiftContentY }) => {
 		if (!$blur) return `
 				min-height: 100%;
 		`
@@ -27,7 +27,7 @@ const AdultContent = styled.div`
 				overflow: ${overflow};
 				> div {
 						height: 100%;
-						transform: translateY(-${$translateContent}px);
+						transform: translateY(-${$shiftContentY}px);
 				};
 				header > div {
 						opacity: 0;
@@ -44,26 +44,29 @@ export default function App() {
 		const { ageConfirmation } = useAgeConfirmationContext()
 		const { contactForm } = useContactFormContext()
 		const { screen, setScreen } = useScreenContext()
+		const contentRef = useRef(null)
 		const displayModalFilter = !ageConfirmation || contactForm
-		const displayModalMask = screen?.scrollTop !== null && displayModalFilter
 		let prevScrollTop = screen?.scrollTop
+		const displayModalMask = screen?.scrollTop !== null && displayModalFilter
+		const shiftContentY = (displayModalMask && screen?.scrollTop) || 0
 
 		const adultContentProps = {
 				$blur: displayModalFilter ? 5 : 0,
-				overflow: displayModalMask ? 'hidden' : 'inherit'
+				overflow: displayModalMask ? 'hidden' : 'inherit',
+				$shiftContentY: shiftContentY,
+				shiftcontenty: shiftContentY,
+				ref: contentRef
 		}
-
-		if (displayModalMask && screen?.scrollTop) adultContentProps.$translateContent = prevScrollTop
 
 		useEffect(() => {
 				if (!displayModalFilter) {
-						const prevScrollTop = screen?.scrollTop
 						setScreen({ ...screen, scrollTop: null })
 						if (prevScrollTop) window.scrollTo(0, prevScrollTop)
 				} else {
-						//const { scrollTop } = document.documentElement
-						//console.log(scrollTop, screen?.scrollTop)
-						//if (scrollTop) setScreen({ ...screen, scrollTop })
+						const scrollTop = document.documentElement.scrollTop || prevScrollTop
+						if (!scrollTop) return
+						setScreen({ ...screen, scrollTop })
+						prevScrollTop = scrollTop
 				}
 		}, [displayModalFilter, screen?.scrollTop])
 		
@@ -77,17 +80,15 @@ export default function App() {
 				}
 
 				const onResize = () => {
-						const { clientHeight, clientWidth, scrollHeight, scrollTop: top } = document.documentElement
-						const result = { scrollTop: prevScrollTop, width: clientWidth, height: clientHeight }
-						prevScrollTop = top || prevScrollTop
+						const { clientHeight, clientWidth, scrollHeight, } = document.documentElement
+						let contentShiftY = +contentRef?.current?.attributes?.getNamedItem('shiftcontenty')?.value
 						const contentHeight = document.getElementsByClassName('react-parallax')[0]?.clientHeight || scrollHeight
 						const scrollTopMax = contentHeight - clientHeight
-						console.log(`screen.scrollTop: ${screen.scrollTop}, result.scrollTop: ${result.scrollTop}, scrollTopMax: ${scrollTopMax}`);
-						if (result.scrollTop > scrollTopMax) {
-								result.scrollTop = scrollTopMax
-
+						if (contentShiftY > scrollTopMax) {
+								contentShiftY = scrollTopMax
 						}
-						setScreen(result)
+						setScreen({ scrollTop: contentShiftY, width: clientWidth, height: clientHeight })
+						if (contentShiftY) prevScrollTop = contentShiftY
 				}
 
 				window.addEventListener('resize', debounce(onResize))
